721381b9a641fffa8bb43f54614794ef8a5be7c1
aevam shingala <aevamshingala97@gmail.com>
2025-01-22 22:46:20 +0530

    big change

---
+++ 4sum.js
@@ -0,0 +1,4 @@
+let arr = [1, -5, 2, 0, 0, -1, -2];
+const target = 0;
+
+// let a = [arr[i] + arr[j]];
--- stringToInt.js
+++ stringToInt.js
@@ -1,17 +1,21 @@
-let a = " 3 !#@$ 234  @#%^*&456  sfdds00001506 dsf 1546 15 1 5 2";
+let a = " 000000000003 10005 !#@$ 234  @";
 let val = "";
+let temp = false;
 
 function stringToInt() {
   for (let i = 0; i < a.length; i++) {
+    a[i] == " " ? (a[i + 1] != 0 ? (temp = true) : (temp = false)) : null;
+
     if (a[i] == "0") {
       if (
-        !isNaN(a[i + 1]) &&
-        !isNaN(a[i - 1]) &&
-        a[i + 1] != 0 &&
-        a[i - 1] != 0
-      )
+        (!isNaN(a[i + 1]) &&
+          !isNaN(a[i - 1]) &&
+          a[i + 1] != 0 &&
+          a[i - 1] != 0) ||
+        temp
+      ) {
         val += a[i];
-      else continue;
+      } else continue;
     } else {
       if (a[i] == " ") continue;
       else {
