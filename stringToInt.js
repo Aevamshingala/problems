dbdf40ec7050519688294184597e77d90ec06b58
aevam shingala <162999531+Aevamshingala@users.noreply.github.com>
2025-01-23 08:05:01 +0530

    condition change

--- stringToInt.js
+++ stringToInt.js
@@ -1,12 +1,18 @@
-let a = " 000000000003 0010005 !#@$ 234  @";  // output := 30010005                                              
+// let a = " 00000000000-3 0010005 !#@$ 234  @";
+let a = "456asd0-2";
 let val = "";
 let temp = false;
-
+let flag = true;
 function stringToInt() {
   for (let i = 0; i < a.length; i++) {
     // a[i] == " " ? (a[i + 1] != 0 ? (temp = true) : (temp = false)) : null;
+    if (a[i] == "-" && a[i + 1] >= 0 && flag == true && !(a[i - 1] >= 0)) {
+      val = "-";
+      flag = false;
+      continue;
+    }
     a[i] > 0 ? (temp = true) : null;
-    if (a[i] == "0") {
+    if (a[i] == "0" && flag == true) {
       if (
         (!isNaN(a[i + 1]) &&
           !isNaN(a[i - 1]) &&
@@ -21,9 +27,11 @@ function stringToInt() {
       else {
         if (isNaN(a[i])) return val || 0;
         else val += a[i];
+        if (i == a.length - 1) {
+          return val;
+        }
       }
     }
   }
 }
 console.log(stringToInt());
-
